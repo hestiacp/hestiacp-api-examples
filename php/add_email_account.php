6fda677965f452560bd7be5e865f795b98174b96
vdbhb59 <60728004+vdbhb59@users.noreply.github.com>
2025-03-06 08:47:31 +0000

    Created example API documentation for email account creation & deletion (#2)
    * Created example API document for email account addition
    
    Added API template for email account creation.
    Saw a post, and started checking. Post URL: https://forum.hestiacp.com/t/how-do-i-create-and-delete-mail-accounts-with-the-api/18328
    
    * Created example API document for email account deletion
    
    Added API template for email account deletion.
    Saw a post, and started checking. Post URL: https://forum.hestiacp.com/t/how-do-i-create-and-delete-mail-accounts-with-the-api/18328
    
    * Corrected indentation
    
    NPP caused incorrect indentation earlier
    
    * Corrected indentation
    
    NPP caused incorrect indentation earlier
    
    * Created example API document for email account addition
    
    Added API template for email account creation.
    Saw a post, and started checking. Post URL: https://forum.hestiacp.com/t/how-do-i-create-and-delete-mail-accounts-with-the-api/18328
    
    * Created example API document for email account deletion
    
    Added API template for email account deletion.
    Saw a post, and started checking. Post URL: https://forum.hestiacp.com/t/how-do-i-create-and-delete-mail-accounts-with-the-api/18328
    
    * Corrected per review request
    
    removed `domain_idn` & `HIDE` which was my oversight.
    
    * Removed domain_idn as suggested
    
    * Updated per review
    
    * Update per review
    
    * Rename add_mail_account.php to add_email_account.php
    
    To be in sync with nodejs and email is correct compared to mail in context
    
    * Rename delete_mail_account.php to delete_email_account.php
    
    To be in sync with nodejs and email is correct compared to mail in context

---
+++ php/add_email_account.php
@@ -0,0 +1,44 @@
+<?php
+
+// Server credentials
+$hst_hostname = 'server.hestiacp.com';
+$hst_port = '8083';
+$hst_username = 'admin';
+$hst_password = 'p4ssw0rd';
+$hst_returncode = 'yes';
+$hst_command = 'v-add-mail-account'; // Refer: https://github.com/hestiacp/hestiacp/blob/release/bin/v-add-mail-account & https://hestiacp.com/docs/reference/cli.html#v-add-mail-account
+
+// Domain details
+$username = 'demo';
+$domain = 'demo.hestiacp.com';
+
+// Prepare POST query
+$postvars = array(
+    'user' => $hst_username,
+    'password' => $hst_password,
+    'returncode' => $hst_returncode,
+    'cmd' => $hst_command,
+    'arg1' => $user,
+    'arg2' => $domain
+    'arg3' => $account
+    'arg4' => $password
+    'arg5' +> $quota // {5-unlimited}
+);
+
+// Send POST query via cURL
+$postdata = http_build_query($postvars);
+$curl = curl_init();
+curl_setopt($curl, CURLOPT_URL, 'https://' . $hst_hostname . ':' . $hst_port . '/api/');
+curl_setopt($curl, CURLOPT_RETURNTRANSFER,true);
+curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, false);
+curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, false);
+curl_setopt($curl, CURLOPT_POST, true);
+curl_setopt($curl, CURLOPT_POSTFIELDS, $postdata);
+$answer = curl_exec($curl);
+
+// Check result
+if($answer === 0) {
+    echo "Added new mail account.\n";
+} else {
+    echo "Query returned error code: " .$answer. "\n";
+}
